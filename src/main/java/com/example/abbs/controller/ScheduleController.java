5dd32d28aa48b437cd4572dedd2e8d52c4c07cff
develop_yunju <ilove4903@naver.com>
2024-03-13 17:31:55 +0900

    SampleProect_240313

--- src/main/java/com/example/abbs/controller/ScheduleController.java
+++ src/main/java/com/example/abbs/controller/ScheduleController.java
@@ -63,13 +63,58 @@ public class ScheduleController {
 		// k는 날짜, i는 요일
 		String sdate = null;
 		// 첫번째 주
-		if (startDate != 0) {
+		if (startDate != 0) {		// 지난 달
 			LocalDate prevSunDay = startDay.minusDays(startDate);
 			int prevDay = prevSunDay.getDayOfMonth();
 			int prevMonth = prevSunDay.getMonthValue();
-			int preYear = prevSunDay.getYear();
+			int prevYear = prevSunDay.getYear();
+			for (int i = 0; i < startDate; i++) {
+				sdate = String.format("%d%02d%02d", prevYear, prevMonth, prevDay+i);
+				SchDay sd = new SchDay();
+				sd.setDay(prevDay+i); sd.setDate(i); sd.setSdate(sdate);
+				week.add(sd);
+			}
+		}
+		for (int i = startDate, k = 1; i < 7; i++, k++) {		// 이번 달
+			sdate = String.format("%d%02d%02d", year, month, k);
+			SchDay sd = new SchDay();
+			sd.setDay(k); sd.setDate(i); sd.setSdate(sdate);
+			week.add(sd);
+		}
+		calendar.add(week);
+		
+		// 둘째 주부터 해당월의 마지막 날까지
+		int day = 8 - startDate;
+		for (int k = day, i = 0; k <= lastDay.getDayOfMonth(); k++, i++) {
+			if (i % 7 == 0)
+				week = new ArrayList<>();
+			sdate = String.format("%d%02d%02d", year, month, k);
+			SchDay sd = new SchDay();
+			sd.setDay(k); sd.setDate(i % 7); sd.setSdate(sdate);
+			week.add(sd);
+			if (i % 7 == 6)
+				calendar.add(week);
 		}
 		
+		// 다음 달 1일부터 그주 토요일까지
+		if (lastDate != 6) {
+			LocalDate nextDay = lastDay.plusDays(1);
+			int nextMonth = nextDay.getMonthValue();
+			int nextYear = nextDay.getYear();
+			for (int i = lastDate + 1, k = 1; i < 7; i++, k++) {
+				sdate = String.format("%d%02d%02d", nextYear, nextMonth, k);
+				SchDay sd = new SchDay();
+				sd.setDay(k); sd.setDate(i); sd.setSdate(sdate);
+				week.add(sd);
+			}
+			calendar.add(week);
+		}
+		
+		model.addAttribute("calendar", calendar);
+		model.addAttribute("today", today+"("+date+")");
+		model.addAttribute("year", year);
+		model.addAttribute("month", String.format("%02d", month));
+		model.addAttribute("height", 600 / calendar.size());
 		return "schedule/calendar";
 	}
 	
